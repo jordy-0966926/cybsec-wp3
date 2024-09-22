c9c5cb70e62b9ae6fe878904d540775250434f99
jordy-0966926 <0966926@hr.nl>
2024-09-22 21:31:47 +0200

    refactor: Update Answer class to include get_answers_by_student_id method
    The Answer class in the server/src/lib/models/answer.py file has been updated to include a new method called get_answers_by_student_id. This method retrieves the answers from the database based on the student ID. This change improves the functionality of the Answer class by allowing the retrieval of specific answers for a given student.

--- server/src/lib/models/answer.py
+++ server/src/lib/models/answer.py
@@ -1,11 +1,11 @@
-from flask_login import current_user
-from server.src.lib.models.database_entity import DatabaseEntry
-from server.src.lib.util.database import db_cursor as connect_db
-
 from typing import List
 
+from flask_jwt_extended import get_jwt_identity
+from src.lib.models.database_entity import DatabaseEntry
+from src.lib.util.database import db_cursor as connect_db
 
-class Answer:
+
+class Answer(DatabaseEntry):
     def __init__(self):
         super().__init__()
         self.table_name = 'answers'
@@ -19,9 +19,19 @@ class Answer:
         data = cursor.fetchone()
         return dict(data)
 
-    def insert_answer(self, answers: list) -> None:
+    def get_answers_by_student_id(self, student_id: int) -> List[dict]:
+        """Get the answers by student id."""
+        qry = f"SELECT * FROM {self.table_name} WHERE student_id IS ?"
+        cursor = connect_db()
+        cursor.execute(qry, (str(student_id),))
+        data = [dict(row) for row in cursor.fetchall()]
+        return data
+
+    def insert_answer(self, statement_id: int, student_id: int, prompt_id: int) -> None:
         """Insert an answer into the database."""
-        student_id = current_user.user_id
-        data = [{'statement_id': answer, 'student_id': student_id}
-                for answer in answers]
+        data = [{'statement_id': statement_id,
+                 'student_id': student_id,
+                 'prompt_id': prompt_id}]
         print(data)
+        self.set_table_data(data)
+        self.insert_data_in_db()
