3ac3dd185b328426113c8e48c13c1d0d6a8836e2
jordy-0966926 <0966926@hr.nl>
2024-09-22 21:32:56 +0200

    refactor: Update teacher login endpoint
    The teacher login endpoint in the `__init__.py` file of the `auth/teacher` module has been refactored. The changes include:
    - Removing unnecessary imports and code
    - Updating the endpoint to only accept POST requests
    - Retrieving the username and password from the request JSON
    - Adding validation for the presence of username and password
    - Authenticating the teacher using the `authenticate_teacher` method
    - Returning an access token and username if authentication is successful
    - Returning an error message if authentication fails
    
    These changes improve the security and functionality of the teacher login endpoint.

--- server/src/api/endpoints/auth/teacher/__init__.py
+++ server/src/api/endpoints/auth/teacher/__init__.py
@@ -1,43 +1,23 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash, session
-from flask_login import login_user, logout_user, current_user, login_required
-from urllib.parse import urlparse, urljoin
-from server.src.lib.models.user import Teacher as User
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import create_access_token
+from src.lib.models.user import Teacher
 
 blueprint = Blueprint('teacher', __name__, template_folder='templates')
 
-# Source: https://stackoverflow.com/questions/60532973/how-do-i-get-a-is-safe-url-function-to-use-with-flask-and-how-does-it-work
 
-
-def is_safe_url(target):
-    ref_url = urlparse(request.host_url)
-    test_url = urlparse(urljoin(request.host_url, target))
-    return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
-
-
-@blueprint.route('/teacher', methods=['GET', 'POST'])
+@blueprint.route('/teacher', methods=['POST'])
 def teacher_login():
-    if current_user.is_authenticated:
-        return {'status': 'ok'}
-
     if request.method == 'POST':
-        session_user = User()
-        username = str(request.form.get('username'))
-        password = str(request.form.get('password'))
+        username = request.json.get("username", None)
+        password = request.json.get("password", None)
+        print(username, password)
+        if not username or not password:
+            return jsonify({"message": "Username and password are required"}), 400
 
-        if session_user.authenticate_user(username, password):
-            login_user(session_user, remember=False)
-            # Check for 'next' parameter in the request URL
-            next_page = session.get('next')
-            if not next_page or not is_safe_url(next_page):
-                next_page = url_for('agendas_blueprint.agendas')
+        session_user = Teacher()
 
-            return redirect(next_page)
-
-    return render_template('auth_teacher.jinja')
-
-
-@blueprint.route('/logout')
-@login_required
-def teacher_logout():
-    logout_user()
-    return redirect(url_for('auth_blueprint.user_login'))
+        if session_user.authenticate_teacher(username, password):
+            access_token = create_access_token(identity=username)
+            return jsonify(access_token=access_token, username=username)
+        else:
+            return jsonify({"message": "Invalid credentials"}), 401
