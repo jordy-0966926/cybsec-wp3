1c173eb9246686df3f2c96133b25a946f13615f5
jordy-0966926 <0966926@hr.nl>
2024-09-22 21:32:15 +0200

    refactor: Add student answers endpoint to student API
    This commit adds a new endpoint '/student/answers' to the student API. The endpoint supports both GET and POST methods. The GET method retrieves the answers of the currently authenticated student, while the POST method allows the student to submit a new answer. The implementation includes retrieving the student ID from the JWT token, fetching the answers from the database, and inserting a new answer into the database. This enhancement improves the functionality of the student API by providing access to student answers and the ability to submit new answers.

--- server/src/api/endpoints/student/__init__.py
+++ server/src/api/endpoints/student/__init__.py
@@ -1,12 +1,13 @@
 from flask import Blueprint, jsonify, request
-
-from server.src.lib.models.user import Student
-
+from flask_jwt_extended import get_jwt_identity, jwt_required
+from src.lib.models.answer import Answer
+from src.lib.models.user import Student
 
 blueprint = Blueprint('student', __name__)
 
 # model instances
 model_student = Student()
+model_answer = Answer()
 
 
 @blueprint.route('/student', methods=['GET', 'POST'])
@@ -18,10 +19,31 @@ def students():
             schema.append({
                 "id": row['id'],
                 "student_num": row['student_num'],
-                "name": row['name'],
                 "type": row['mbti'],
                 "class": row['class_id'],
                 "team": row['team_id'],
             })
 
         return jsonify(schema)
+
+
+@blueprint.route('/student/answers', methods=['GET', 'POST'])
+@jwt_required()
+def student_answers():
+    if request.method == "GET":
+        current_user = get_jwt_identity()
+        student_id = model_student.get_user_id_by_studentnum(current_user)
+        data = model_answer.get_answers_by_student_id(student_id=student_id)
+        return jsonify(data)
+
+    if request.method == "POST":
+        student_id = request.json.get('student_id')
+        statement_id = request.json.get('statement_id')
+        pompt_id = request.json.get('prompt_id')
+        model_answer.insert_answer(
+            statement_id=statement_id, student_id=student_id, prompt_id=pompt_id)
+
+        if not student_id or not statement_id:
+            return jsonify({"message": "Student ID and Statement ID are required"}), 400
+
+    return jsonify({"message": "Student answers received"}), 200
